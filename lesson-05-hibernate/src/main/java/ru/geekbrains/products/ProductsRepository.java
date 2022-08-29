9ceea5f61cc4ca2d7999bb97c945c26e325c4ac9
Nikolai Kostromitin <emailbin@yandex.ru>
2022-08-29 15:29:45 +0500

    Remove unnecessary transactions and refactor common code to separate methods

--- lesson-05-hibernate/src/main/java/ru/geekbrains/products/ProductsRepository.java
+++ lesson-05-hibernate/src/main/java/ru/geekbrains/products/ProductsRepository.java
@@ -1,48 +1,65 @@
 package ru.geekbrains.products;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 import ru.geekbrains.products.model.Product;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 @RequiredArgsConstructor
 public class ProductsRepository {
-    private final EntityManager entityManager;
+    private final EntityManagerFactory entityManagerFactory;
 
-    public Product findById(long id) {
-        entityManager.getTransaction().begin();
-        Product product = entityManager.find(Product.class, id);
-        entityManager.getTransaction().commit();
-        return product;
+    public Optional<Product> findById(long id) {
+        return execute(entityManager -> Optional.ofNullable(entityManager.find(Product.class, id)));
     }
 
     public List<Product> findAll() {
-        entityManager.getTransaction().begin();
-        List<Product> products = entityManager.createNamedQuery("Product.findAll", Product.class)
-                .getResultList();
-        entityManager.getTransaction().commit();
-        return products;
+        return execute(entityManager -> entityManager.createNamedQuery("Product.findAll", Product.class)
+                .getResultList());
     }
 
     public void deleteById(long id) {
-        entityManager.getTransaction().begin();
-        entityManager.createNamedQuery("Product.deleteById")
-                        .setParameter("id", id)
-                                .executeUpdate();
-        entityManager.getTransaction().commit();
+        executeInTransaction(entityManager -> {
+            entityManager.createNamedQuery("Product.deleteById")
+                    .setParameter("id", id)
+                    .executeUpdate();
+        });
     }
 
-    public Product saveOrUpdate(Product product) {
-        entityManager.getTransaction().begin();
-        Product entity = entityManager.merge(product);
-        entityManager.getTransaction().commit();
-        return entity;
+    public void saveOrUpdate(Product product) {
+        executeInTransaction(entityManager -> {
+            if (product.getId() != null) {
+                entityManager.persist(product);
+            } else {
+                entityManager.merge(product);
+            }
+        });
     }
 
-    public void insert(Product product) {
-        entityManager.getTransaction().begin();
-        entityManager.persist(product);
-        entityManager.getTransaction().commit();
+    private <R> R execute(Function<EntityManager, R> function) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        try {
+            return function.apply(em);
+        } finally {
+            em.close();
+        }
+    }
+
+    private void executeInTransaction(Consumer<EntityManager> consumer) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            consumer.accept(em);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
     }
 }
